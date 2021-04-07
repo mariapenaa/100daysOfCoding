09940114fc97d0ead3feff8f02b72be56cca7dc0
Maria Peña <mpena@udesa.edu.ar>
2021-04-07 18:24:09 -0300

    principio del quiz app

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ Consiste en programar  1 hora al día durante 100 días.
 ## Proyectos
 Voy a ir creando distitnos proyectos para aplicar mis conocimientos de HTML, CSS, JavaScript, y React
 
+1. Calculadora con javascript
+2. todo list con React.js, me guie mucho por un tutorial de https://mauriciogc.medium.com/react-creando-una-app-to-do-list-con-create-react-app-y-el-hook-usestate-6ae378569705
+3. quiz app con React.js
--- quizapp/src/App.js
+++ quizapp/src/App.js
@@ -1,23 +1,67 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const questions = [
+		{
+			questionText: 'What is the capital of France?',
+			answerOptions: [
+				{ answerText: 'New York', isCorrect: false },
+				{ answerText: 'London', isCorrect: false },
+				{ answerText: 'Paris', isCorrect: true },
+				{ answerText: 'Dublin', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'Who is CEO of Tesla?',
+			answerOptions: [
+				{ answerText: 'Jeff Bezos', isCorrect: false },
+				{ answerText: 'Elon Musk', isCorrect: true },
+				{ answerText: 'Bill Gates', isCorrect: false },
+				{ answerText: 'Tony Stark', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'The iPhone was created by which company?',
+			answerOptions: [
+				{ answerText: 'Apple', isCorrect: true },
+				{ answerText: 'Intel', isCorrect: false },
+				{ answerText: 'Amazon', isCorrect: false },
+				{ answerText: 'Microsoft', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'How many Harry Potter books are there?',
+			answerOptions: [
+				{ answerText: '1', isCorrect: false },
+				{ answerText: '4', isCorrect: false },
+				{ answerText: '6', isCorrect: false },
+				{ answerText: '7', isCorrect: true },
+			],
+		},
+	];
+
+  //What we want to do is use a state object to hold which question the user is currently on, 
+  //and update this when an answer button is clicked. You can see this from running the code in the final example.
+  const [currentQuestion, setCurrentQuestion] = useState(0)
+  const [answer, setAnswer] = useState();
+  const [score, setScore] = useState(); 
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <p>Quiz app</p>
+
+      <div className='question-text'>{questions[currentQuestion].questionText}</div>
+      <div className='answer-section'>
+        {questions[currentQuestion].answerOptions.map((answerOption, index)=>(
+            <button>{answerOption.answerText}</button>
+        ))};
+      </div>
+
+
     </div>
   );
 }
