dc8f21f3f0256dd1de68e795c98c430431e7b9e4
Maria Peña <mpena@udesa.edu.ar>
2021-04-11 20:32:34 -0300

    Cree react app para recioe

---
+++ recipe-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = () =>{
+  const APP_ID = '9c8c596f'
+  return (
+    <div className="App">
+      <h1>Hello React</h1>
+    </div>
+  )
+}
+
+export default App;
